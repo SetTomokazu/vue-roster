5af6374207f76b5ebcb003bad909174c38a7a7d3
SetTomokazu <set.tomokazu@gmail.com>
2020-03-04 07:49:36 +0900

    とりあえずの編集箇所追加

---
+++ src/views/Roster.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="roster">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Roster extends Vue {
+  private targetMonth: Date = new Date();
+  private workRecord: [] = [];
+
+  private get target() {
+    return this.targetMonth !== null ? this.targetMonth : new Date();
+  }
+
+  private mounted() {
+    console.log(this.target);
+    const d = this.target;
+
+    const firstDate = this.getFirstDay(d);
+    const lastDate = this.getLastDay(d);
+    console.log(firstDate);
+    console.log(lastDate);
+  }
+
+  private getFirstDay(src: Date) {
+    return new Date(src.getFullYear(), src.getMonth(), 1);
+  }
+
+  private getLastDay(src: Date) {
+    const nextMonth = new Date(src);
+    nextMonth.setMonth(src.getMonth() + 1);
+    nextMonth.setDate(0);
+    return nextMonth;
+  }
+}
+</script>
