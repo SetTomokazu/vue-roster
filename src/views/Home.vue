20cab2b62a35678e5f86b68be6c07a86d73447a1
SetTomokazu <set.tomokazu@gmail.com>
2020-04-02 22:47:11 +0900

    sonarqubeがうっとうしいので削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue";
 import RosterExporter from "@/components/lib/RosterExporter";
 
-@Component({ components: { HelloWorld } })
+@Component({ components: {} })
 export default class Home extends Vue {
   private mounted() {
     RosterExporter.export();
